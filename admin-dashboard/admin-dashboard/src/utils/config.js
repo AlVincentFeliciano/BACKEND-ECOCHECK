c5ae572dcea4315bece1a7d0728b0e2f0cff01eb
AlVincentFeliciano <ydcbgamingjew90@gmail.com>
2026-03-14 16:56:11 +0800

    Point admin dashboard to new backend and refresh bundled build

--- admin-dashboard/admin-dashboard/src/utils/config.js
+++ admin-dashboard/admin-dashboard/src/utils/config.js
@@ -1,4 +1,4 @@
 // Temporarily use production URL until local backend is running
-export const API_URL = "https://backend-ecocheck.onrender.com/api";
+export const API_URL = "https://backend-ecocheck-9wvo.onrender.com/api";
 
 console.log('Current API URL:', API_URL);
